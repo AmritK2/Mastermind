cf3e0374a33939a2f17a21c1925c90d0c46f4a01
Amrit Kaur <Amrit.Kaur@myob.com>
2017-12-07 10:57:32 +1300

    test commit

---
+++ test.js
@@ -0,0 +1,7 @@
+var expect = require('chai').expect; // gives access to files
+
+describe("TestClassShould", function() {
+   it("return true", function() {
+       expect(true).to.equal(false);
+   });
+});
